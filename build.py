6e767ef836780f685392aa807a586b3d4199172c
kiri <krclemmensen@gmail.com>
2021-10-03 14:31:36 -0400

    final changes to build.py

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+# list of pages for website
+
 pages = [
 	{
 		"filename": "content/index.html",
@@ -21,22 +23,26 @@ pages = [
 	}
 ]
 
-def apply_template(content):
+# opens the template page and replaces the content for each page
+
+def apply_template(content, title):
 	template = open('templates/base.html').read()
 	result = template.replace('{{content}}', content)
-	return result
+	new_result = apply_title(result, title)
+	return new_result
 
-def apply_title(title):	
-	template = open('templates/base.html').read()
-	result = template.replace('{{title}}', title)
+# replaces the title so that the browswer displays the correct title for each page 
+def apply_title(result, title):	
+	result = result.replace('{{title}}', title)
 	return result				
-	
+
+# loops through each entry in pages list, saves output 
+
 def main():
 	for page in pages:
 		content = open(page['filename']).read()
 		title = page['title']
-		fin_page = apply_template(content)
-#		fin_page = apply_title(title)
+		fin_page = apply_template(content, title)
 		open(page['output'], 'w+').write(fin_page)
 
 main()
